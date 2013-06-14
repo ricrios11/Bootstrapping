92a939643fe3e88e170584417542612108be5f0f
ricrios11 <rexk5c@gmail.com>
2013-06-13 21:27:18 -0400

    added dependancy for pgp

--- modules/people/manifests/ricrios11.pp
+++ modules/people/manifests/ricrios11.pp
@@ -23,7 +23,8 @@ class people::ricrios11 {
     include vagrant
     include virtualbox
     include vlc
-    include php::5_4
+    include wget
+    #include php::5_4
  
     # Machine-specific apps
     case $::hostname {
@@ -67,40 +68,5 @@ class people::ricrios11 {
       node_version => 'v0.10'
     }
 
-    # Install a php version and set as the global default php
-    class { 'php::global':
-    version => '5.4.10'
-    }
-
-    # Ensure a specific php version is used within a directory
-    php::local { '/path/to/my/awesome/project':
-    version => '5.4.9'
-    }
-
-    # Ensure an extension is installed for a certain php version
-    # note, you can't have duplicate resource names so you have to name like so
-    php::extension::apc { "apc for ${version}":
-    php     => $version,
-    version => '3.1.13', # Optionally specify the extension version
-    }
-
-    # Set up PHP-FPM as a service running a specific version of PHP
-    include php::fpm::5_3_15
-
-    # Run multiple PHP-FPM services
-    include php::fpm::5_4_11
-    include php::fpm::5_3_23
-
-    # Spin up a PHP-FPM pool for a project
-    # Ensures:
-    #  * the version of PHP is installed
-    #  * a PHP-FPM service is configured for this PHP version
-    #  * a FPM pool is listening on a per project nginx socket
-    $name = "project-name"
-    $version = "5.4.10"
-    php::fpm::pool { "${name}-${version}":
-    version     => $version,
-    socket_path => "${boxen::config::socketdir}/${name}",
-    require     => File["${nginx::config::sitesdir}/${name}.conf"],
-    }
+    
 }
