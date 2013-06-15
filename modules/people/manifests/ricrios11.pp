b811ed8ef54208dea9e2dcbcbf27e67d14fdd8cc
ricrios11 <rexk5c@gmail.com>
2013-06-15 09:42:10 -0400

    dependancy

--- modules/people/manifests/ricrios11.pp
+++ modules/people/manifests/ricrios11.pp
@@ -118,4 +118,14 @@ class people::ricrios11 {
     nodejs::module { 'bower':
     node_version => 'v0.10'
     }
+
+    repository {
+    '/path/to/code':
+    source   => 'user/repo', #short hand for github repos
+    provider => 'git';
+    'my emacs config':
+    source   => 'git://github.com/wfarr/.emacs.d.git',
+    path     => '/etc/emacs.d',
+    provider => 'git',
+}
 }
